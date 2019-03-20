e2dcb017fe54bbe55906b95f3b1dc3b830d8f593
johnwilhite <jbwilhite@gmail.com>
2019-03-20 11:02:16 -0400

    move season urls to json file

--- 19-fresh-meat-ii/README.md
+++
@@ -1,8 +0,0 @@
-For some reason this season doesn't get the proper audio using the default settings (mp4).  Use the `--flv` option instead.
-``` bash
-./dl.rb --flv 19
-
-# Then change to season directory and convert to mp4
-for i in *.flv; do name=`echo $i | cut -d'.' -f1`; ffmpeg -i "$i" "${name}.mp4"; done;
-rm *.flv
-```
--- dl.rb
+++ dl.rb
@@ -1,59 +1,56 @@
 #!/usr/bin/env ruby
 require 'fileutils'
-require 'optparse'
+require 'json'
 
-season = ARGV.last
-flv = false
-ext = '.mp4'
-dl_options = ''
+arg = ARGV.last.dup
+file = File.open 'seasons.json'
+seasons = JSON.parse(file.read)
 
-# get options
-OptionParser.new do |o|
-    o.on('--flv', 'FLV') { flv = true }
-    o.parse!
+# regex match argument to a season
+selection = seasons.find {|season, episodes| /#{arg}.*/.match season}
+
+# if season not found
+if not selection
+	puts 'Season info not found, try one of the following:'
+	puts seasons.keys
+	exit
 end
 
-if flv
-  ext = '.flv'
-  dl_options += '-f hds-2048 '
-end
+season = selection[0]
+episodes = selection[1]
 
-# match to season directory and cd if it exists
-dir = Dir.glob("#{season}*").first.to_s
-abort 'Season directory not found' unless !dir.empty? && Dir.chdir(dir)
+# make directory and change to it
+FileUtils.mkdir_p season
+Dir.chdir season
 
-File.open('list.txt', 'r') do |list|
-  list.each_line do |line|
-    puts line
+# download each episode
+episodes.each do |episode|
+	file_name = episode[1]
+	url = episode[0]
+	ext = file_name.partition('.').last.prepend('.')
+	dl_options = ext == '.flv' ? '-f hds-2048 ' : ''
 
-    # create file name as S##E##
-    # if line contains # use everything after that as ep name, else use last two chars
-    line = line.gsub("\n", '')
-    season_padded = season.to_s.rjust(2, '0')
-    episode_padded = line.include?('#') ? line.partition('#').last : line.chars.last(2).join
-    file_name = "S#{season_padded}E#{episode_padded}#{ext}"
+	next puts "#{file_name} already exists" if File.exist? file_name
 
-    next if File.exist? file_name
+	# download files to temp dir
+	# mtv downloads come out in multiple pieces
+	# autonumber to preserve order
+	FileUtils.rm_r 'temp' if Dir.exists? 'temp'
+	FileUtils.mkdir 'temp'
 
-    # download files to temp dir
-    # mtv downloads come out in multiple pieces
-    # autonumber to preserve order
-    FileUtils.rm_r 'temp' if Dir.exists? 'temp'
-    FileUtils.mkdir 'temp'
+	Dir.chdir 'temp' do
+		puts "\n\n\nDownloading #{url} \r\n\n"
+		system "youtube-dl #{dl_options} -o \"%(autonumber)s.%(ext)s\" #{url}"
 
-    Dir.chdir 'temp' do
-      system "youtube-dl #{dl_options} -o \"%(autonumber)s.%(ext)s\" #{line}"
-      
-      # make list file of all partials
-      Dir.glob("*#{ext}").sort.each do |partial|
-        open('partials.txt', 'a') do |partials_list_line|
-          partials_list_line.puts "file '#{partial}'"
-        end
-      end
-    end
+		# make list file of all partials
+		Dir.glob("*#{ext}").sort.each do |partial|
+			open('partials.txt', 'a') do |partials_list_line|
+				partials_list_line.puts "file '#{partial}'"
+			end
+		end
+	end
 
-    # use ffmpeg to concat the partials
-    system "</dev/null ffmpeg -f concat -safe 0 -i temp/partials.txt -c copy #{file_name}"
-    FileUtils.rm_r 'temp'
-  end
+	# use ffmpeg to concat the partials
+	system "</dev/null ffmpeg -f concat -safe 0 -i temp/partials.txt -c copy #{file_name}"
+	FileUtils.rm_r 'temp'
 end
